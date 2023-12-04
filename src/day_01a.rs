5c773aa63c9fb92a08a0a07c6ad4007892d48dea
Meisterlala <6453306+Meisterlala@users.noreply.github.com>
2023-12-04 13:03:28 +0100

    Refactor test function names

--- src/day_01a.rs
+++ src/day_01a.rs
@@ -22,7 +22,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_individual() {
+    fn individual() {
         assert_eq!(solve("1abc2"), 12);
         assert_eq!(solve("pqr3stu8vwx"), 38);
         assert_eq!(solve("a1b2c3d4e5f"), 15);
@@ -30,7 +30,7 @@ mod tests {
     }
 
     #[test]
-    fn test_multiple() {
+    fn multiple() {
         assert_eq!(solve("1abc2\npqr3stu8vwx\na1b2c3d4e5f\ntreb7uchet"), 142);
     }
 }
